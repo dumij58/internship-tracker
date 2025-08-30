fabd8d57a5e5f01a5001197e415b969dafe08e45
dumij58 <dumij58@gmail.com>
2025-08-30 21:30:43 +0530

    Fix footer brand name

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     </div>
 
     <footer>
-        <p>&copy; <?php echo date('Y'); ?> Internship Application & Tracking System. All Rights Reserved.</p>
+        <p>&copy; <?php echo date('Y'); ?> InternSphere. All Rights Reserved.</p>
     </footer>
 
 </body>
