31bc93edaa13065d32344f81b59b7d96e6c7b463
dumij58 <dumij58@gmail.com>
2025-09-04 18:47:57 +0530

    Add sample php files to all essential pages

---
+++ pages/company/post_internship.php
@@ -0,0 +1,7 @@
+<?php
+include_once '../../includes/header.php';
+?>
+
+<p>This page allows companies to post new internship opportunities.</p>
+
+<?php include_once '../../includes/footer.php'; ?>
---
+++ pages/functionalities.php
@@ -0,0 +1,7 @@
+<?php
+include_once '../includes/header.php';
+?>
+
+<p>This page outlines the various functionalities available on the platform.</p>
+
+<?php include_once '../includes/footer.php'; ?>
